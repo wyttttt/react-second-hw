d0c9b4ced380eb8eb6949c1fa65f42ccd97e19fa
王宇婷 <44824987+wyttttt@users.noreply.github.com>
2020-10-18 16:11:31 +0800

    Signed-off-by: 王宇婷 <44824987+wyttttt@users.noreply.github.com>

---
+++ build/precache-manifest.3e1011cdd63dce8ec362f5b66015c327.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c26eba21c0b1651138e5ff4e3231959c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1fb3992a37374bbe644f",
+    "url": "/static/css/main.7032b9f2.chunk.css"
+  },
+  {
+    "revision": "b295a69c990d6d682720",
+    "url": "/static/js/2.43bc3342.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.43bc3342.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1fb3992a37374bbe644f",
+    "url": "/static/js/main.dba6b1de.chunk.js"
+  },
+  {
+    "revision": "967bfd57e19f8ac017fc",
+    "url": "/static/js/runtime-main.1072f285.js"
+  }
+]);
